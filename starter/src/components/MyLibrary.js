0e8b15b5ea2af47f96191dcd2551127104fe654e
shaher.je <shaher.jamaleldin@gmail.com>
2023-03-09 14:56:49 +0100

    disassembled the given HTML code into the components creating the initial separation

--- starter/src/components/MyLibrary.js
+++ starter/src/components/MyLibrary.js
@@ -1,5 +1,22 @@
+import Shelf from "./Shelf";
+
 const MyLibrary = () => {
 
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+
+            <div className="list-books-content">
+                <Shelf></Shelf>
+            </div>
+
+            <div className="open-search">
+                <a onClick="">Add a book</a>
+            </div>
+        </div>
+    );
 
 }
 
--- starter/src/components/SearchBooks.js
+++ starter/src/components/SearchBooks.js
@@ -1,5 +1,25 @@
 const SearchBooks = () => {
-
+    return (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a
+              className="close-search"
+              onClick=""
+            >
+              Close
+            </a>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title, author, or ISBN"
+              />
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+    );
 }
 
 export default SearchBooks;
